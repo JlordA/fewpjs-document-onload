6cac8c16ed493d6abcfa042f1c9678cd554739bd
JlordA <james.ardery@gmail.com>
2020-11-17 23:15:18 -0500

    difficult to figure out they want function to fire on DOM Content Loaded

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    updateDOM()
+
+  }); 
+function updateDOM(){
+    const text = document.querySelector('#text')
+    text.innerHTML = "This is really cool!"
+}
